6df3dd78335a306c05b1e13cbfdf3f34a5fa0ebc
pawelzysk <pawelzysk1989@gmail.com>
2022-10-02 10:48:53 +0200

    wip

---
+++ src/05117-medium-without.ts
@@ -0,0 +1,18 @@
+type WithoutItem<Items extends any[], Item> = Items extends [
+  infer Head,
+  ...infer Tail
+]
+  ? Head extends Item
+    ? WithoutItem<Tail, Item>
+    : [Head, ...WithoutItem<Tail, Item>]
+  : [];
+
+type Without<T extends any[], U> = U extends [infer Head, ...infer Tail]
+  ? WithoutItem<Without<T, Tail>, Head>
+  : U extends []
+  ? T
+  : WithoutItem<T, U>;
+
+type Res = Without<[1, 2], 1>; // expected to be [2]
+type Res1 = Without<[1, 2, 4, 1, 5], [1, 2]>; // expected to be [4, 5]
+type Res2 = Without<[2, 3, 2, 3, 2, 3, 2, 3], [2, 3]>; // expected to be []
